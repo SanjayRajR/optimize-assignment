a65b92a1d144b962f4eeeb093c7c0b1b488ec591
Sanjay Raj R <sanjayrajr@Sanjays-Laptop.local>
2025-05-24 02:00:37 +0530

    Added navbar background on scroll

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -1,12 +1,29 @@
+"use client"
+
 import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
 
 const styles = {
   navLinks: "text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white transition-colors"
 }
 
 export default function AppBar() {
+
+  const [navBg, setNavBg] = useState(false);
+  
+  const changeNavBg = () => {
+    window.scrollY >= 60 ? setNavBg(true) : setNavBg(false);
+   }
+ 
+   useEffect(() => {
+     window.addEventListener('scroll', changeNavBg);
+     return () => {
+       window.removeEventListener('scroll', changeNavBg);
+     }
+   }, [])
+
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50  bg-transparent">
+    <nav className={`fixed top-0 left-0 right-0 z-50 ${ navBg && "bg-black"}`}>
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <Link
